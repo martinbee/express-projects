435386bbb2aaa501e6559de7b3ab2cc4a061a058
Martin Bee <martbee88@gmail.com>
2017-08-22 12:14:37 -0500

    feat: simple express server

--- express-webserver/app.js
+++ express-webserver/app.js
@@ -1,14 +1,23 @@
-const http = require('http');
+const express = require('express');
 
-const hostname = '127.0.0.1';
+const app = express();
 const port = 3000;
 
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World\n');
+app.get('/', (req, res) => {
+  res.send('Hello World');
 });
 
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
+app.get('/about', (req, res) => {
+  const html = '<h1>Hello World</h1>';
+
+  res.send(html);
 });
+
+app.get('/users/:name', (req, res) => {
+  const { name } = req.params;
+  const html = `<h1>Hello ${name}</h1>`;
+
+  res.send(html);
+});
+
+app.listen(port, () => console.log(`Server started on port ${port}`));
